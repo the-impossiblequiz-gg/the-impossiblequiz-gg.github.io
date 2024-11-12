450c470ab7dfc6c863bbe4c8fb44a0de519cb938
Macbook Pro <macbookpro@stinsons-mac.home>
2024-11-12 14:52:19 +0500

    cat bugs

--- js/custom.js
+++ js/custom.js
@@ -8,15 +8,15 @@ This file will not be overwritten by the updater
 
 // JavaScript code
 function search_animal() {
-  let input = document.getElementById("searchbar").value;
-  input = input.toLowerCase();
-  let x = document.getElementsByClassName("animals");
+ Play Online at the-impossiblequiz-gg.github.io</title>let input = document.getElementById("searchbar").value;
+ Play Online at the-impossiblequiz-gg.github.io</title>input = input.toLowerCase();
+ Play Online at the-impossiblequiz-gg.github.io</title>let x = document.getElementsByClassName("animals");
 
-  for (i = 0; i < x.length; i++) {
-    if (!x[i].innerHTML.toLowerCase().includes(input)) {
-      x[i].style.display = "none";
-    } else {
-      x[i].style.display = "block";
-    }
-  }
+ Play Online at the-impossiblequiz-gg.github.io</title>for (i = 0; i < x.length; i++) {
+ Play Online at the-impossiblequiz-gg.github.io</title> Play Online at the-impossiblequiz-gg.github.io</title>if (!x[i].innerHTML.toLowerCase().includes(input)) {
+ Play Online at the-impossiblequiz-gg.github.io</title> Play Online at the-impossiblequiz-gg.github.io</title> Play Online at the-impossiblequiz-gg.github.io</title>x[i].style.display = "none";
+ Play Online at the-impossiblequiz-gg.github.io</title> Play Online at the-impossiblequiz-gg.github.io</title>} else {
+ Play Online at the-impossiblequiz-gg.github.io</title> Play Online at the-impossiblequiz-gg.github.io</title> Play Online at the-impossiblequiz-gg.github.io</title>x[i].style.display = "block";
+ Play Online at the-impossiblequiz-gg.github.io</title> Play Online at the-impossiblequiz-gg.github.io</title>}
+ Play Online at the-impossiblequiz-gg.github.io</title>}
 }
